9509523a791a68a6bfde44e53ed39df5e17f194e
Rafael Chicovis <rafaelkaua97@gmail.com>
2024-10-03 22:16:38 -0300

    ...

---
+++ Backend/RafaelChicovisBlog/Migrations/20241004005339_Creating_Posts.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RafaelChicovisBlog.Migrations
+{
+    /// <inheritdoc />
+    public partial class Creating_Posts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                schema: "public",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    PostDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    PostDownloadUrl = table.Column<string>(type: "character varying(450)", maxLength: 450, nullable: false),
+                    ThumbnailUrl = table.Column<string>(type: "character varying(450)", maxLength: 450, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts",
+                schema: "public");
+        }
+    }
+}
